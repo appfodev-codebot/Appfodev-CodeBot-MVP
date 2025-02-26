378684bc682cc7de5e06c365adf4ebd088d0a351
Mohd Wasil <appfodev666@gmail.com>
2025-02-27 00:23:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to Appfodev CodeBot MVP Backend"}
+print("Welcome to Appfodev CodeBot MVP")
